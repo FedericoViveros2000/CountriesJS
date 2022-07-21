57ccaa2d41e273d862ab695c04aa5b4b191443ff
Jose Federico Viveros Marin <josevivers2000@gmail.com>
2022-07-21 14:06:49 -0400

    Bug en la seleccion solucionado, y agregado la opcion de poder elegir una frontera para ver sus detalles

--- js/modules/displayCountrySelected.js
+++ js/modules/displayCountrySelected.js
@@ -1,2 +1,25 @@
-let createViewSelected=c=>{$containerSearch.classList.add("hide");let{name:b,population:d,region:e,subregion:f,currencies:g,languages:h,flags:i,capital:j}=c[0],a=document.createElement("article");a.classList.add("article"),a.innerHTML=`
-<button class=article__back><a href=index.html>Back</a></button><figure class=article__flag><img class=article__img src=${i.svg}></figure><div class=article__data><section class=article__data--one><h1 class=article__title>${b.common}</h1><p><span class=article__span>Native Name: </span>${b.common}<p><span class=article__span>Population: </span>${d}<p><span class=article__span>Region: </span>${e}<p><span class=article__span>Sub Region: </span>${f}<p><span class=article__span>Capital: </span>${j[0]}</section><section class=article__data--two><p><span class=article__span>Top level Domain</span><p><span class=article__span>Currencies: </span>${Object.values(g)[0].name}<p><span class=article__span>Lenguajes: </span>${Object.values(h)}</section></div>`,fragment.appendChild(a),$containerCountry.appendChild(fragment)};export{createViewSelected}
+let createViewSelected=d=>{$containerSearch.classList.add("hide");let{name:b,population:e,region:f,subregion:g,currencies:h,languages:i,flags:j,capital:k,tld:l,borders:c}=d[0],a=document.createElement("article");a.classList.add("article"),a.innerHTML=`
+            <button class="article__back"><a href="index.html">Home</a></button>
+            <figure class="article__flag">
+                <img class="article__img" src="${j.svg}"/>
+            </figure>
+            <div class="article__data">
+            <h1 class="article__title">${b.common}</h1>
+                <section class="article__data--one">
+                    <p><span class="article__span">Native Name: </span>${b.common}</p>
+                    <p><span class="article__span">Population: </span>${new Intl.NumberFormat().format(e)}</p>
+                    <p><span class="article__span">Region: </span>${f}</p>
+                    <p><span class="article__span">Sub Region: </span>${g}</p>
+                    <p><span class="article__span">Capital: </span>${k[0]}</p>
+                </section>
+                <section class="article__data--two">
+                    <p><span class="article__span">Top level Domain: </span>${l[0]}</p>
+                    <p><span class="article__span">Currencies: </span>${Object.values(h)[0].name}</p>
+                    <p><span class="article__span">Lenguajes: </span>${Object.values(i)}</p>
+                </section>
+                ${void 0!=c?`<section class="article__data--borders">
+                        <p class="article__span">Border Countries: </p>
+                        ${c.map(a=>`<button class="article__border"><a href="#/detailsBorder/${a}">${a}</a></button>`)}
+                    </section>`:""}
+            </div>
+        `,fragment.appendChild(a),$containerCountry.appendChild(fragment)};export{createViewSelected}
--- js/modules/infinityScroll.js
+++ js/modules/infinityScroll.js
@@ -1,1 +1,22 @@
-let start=0,options={root:null,rootMargin:"100px 0px",threshold:.3},loadingSqueleton=()=>{for(let b=0;b<8;b++){let a=document.createElement("article");a.classList.add("squeleton__article"),a.innerHTML=`<figure class=squeleton__image></figure><p class=squeleton__text><p class=squeleton__text><p class=squeleton__text><p class=squeleton__text><p class=squeleton__text><p class=squeleton__text><p class=squeleton__text>`,fragment.appendChild(a)}$containerCountry.appendChild(fragment)},createArticles=a=>{$containerSearch.classList.remove("hide"),a.map(c=>{let{name:a,flags:d,population:e,region:f}=c,b=document.createElement("article");b.innerHTML=`<article class=container__article id=${a.common}><figure class=container__image><img alt=${a.official} class="img img__load"loading=lazy src=${d.svg}></figure><div class=container__article--text><p class=container__article--title><a class=link href=#/details/${a.official} id=${a.official}>${a.official}</a><p><span class=text-span>Population: </span>${new Intl.NumberFormat().format(e)}<p><span class=text-span>Region: </span>${f}</div></article>`,fragment.appendChild(b)}),$containerCountry.appendChild(fragment),document.querySelectorAll(".img__load").forEach(a=>{a.addEventListener("error",()=>{a.src="./img/imgNotFound.png"})})},createArticlesFiltered=a=>{a.length>0?createArticles(a):$containerCountry.innerHTML=`<div class=notFound><img class=notFound__img src=./img/no-results.png><h1>No se han encontrado resultados</h1></div>`},createObserver=a=>{let b=document.querySelectorAll(".container__article")[document.querySelectorAll(".container__article").length-1];""==$inputSearch.value&&a.observe(b)};export{start,options,createArticles,createObserver,loadingSqueleton,createArticlesFiltered}
+let start=0,options={root:null,rootMargin:"100px 0px",threshold:.3},loadingSqueleton=()=>{for(let b=0;b<8;b++){let a=document.createElement("article");a.classList.add("squeleton__article"),a.innerHTML=`<figure class="squeleton__image">                         
+                          </figure>
+                          <p class="squeleton__text"></p>
+                          <p class="squeleton__text"></p>
+                          <p class="squeleton__text"></p>
+                          <p class="squeleton__text"></p>
+                          <p class="squeleton__text"></p>
+                          <p class="squeleton__text"></p>
+                          <p class="squeleton__text"></p>`,fragment.appendChild(a)}$containerCountry.appendChild(fragment)},createArticles=a=>{$containerSearch.classList.remove("hide"),a.map(c=>{let{name:a,flags:d,population:e,region:f}=c,b=document.createElement("article");b.innerHTML=`<article class="container__article" id="${a.common}">
+                              <figure class="container__image">
+                                <img src="${d.svg}" alt="${a.official}" class="img img__load" loading="lazy">
+                              </figure>
+                              <div class="container__article--text">
+                                  <p class="container__article--title"><a href="#/details/${a.official}" id="${a.official}" class="link">${a.official}</a></p>
+                                  <p><span class="text-span">Population: </span>${new Intl.NumberFormat().format(e)}</p>
+                                  <p><span class="text-span">Region: </span>${f}</p>
+                              </div>
+                          </article>`,fragment.appendChild(b)}),$containerCountry.appendChild(fragment),document.querySelectorAll(".img__load").forEach(a=>{a.addEventListener("error",()=>{a.src="./img/imgNotFound.png"})})},createArticlesFiltered=a=>{a.length>0?createArticles(a):$containerCountry.innerHTML=`
+                           <div class="notFound">
+                               <img src="./img/no-results.png" class="notFound__img"/>
+                               <h1>No results found</>
+                           </div>`},createObserver=a=>{let b=document.querySelectorAll(".container__article")[document.querySelectorAll(".container__article").length-1];""==$inputSearch.value&&a.observe(b)};export{start,options,createArticles,createObserver,loadingSqueleton,createArticlesFiltered}
